068078e4a965d6f3a8209eeb7f0a68a9fa19d83f
Umer <omiomeromi11@gmail.com>
2025-11-12 07:42:05 +0300

    Add content to hero section and organize some utility classes

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -3,41 +3,52 @@ import { ArrowRight, Briefcase, Users } from "lucide-react";
 export default function HeroSection(){
 
     return (
-      <section className="flex bg-linear-to-br from-indigo-50 to-white flex-col gap-y-5 px-4 py-15">
+      <section className="hero-container">
         <div className="space-y-7">
           <div className="space-y-7">
-            <h1 className="text-5xl font-bold leading-14">
+            <h1 className="text-48-bold">
               Welcome to your professional network
             </h1>
-            <p className="text-xl text-gray-500">
+            <p className="text-20-gray">
               Connect with industry leaders, share your expertise, and discover
               opportunities that shape your career journey
             </p>
           </div>
 
           <div className="flex flex-col space-y-4">
-            <button className="group rounded-lg bg-linear-to-r from-blue-300 to-indigo-100 font-semibold py-2 text-lg space-x-1 cursor-pointer hover:from-blue-400 hover:to-indigo-200 transition-colors duration-700">
+            <button className="group hero-btn-signup">
               <span>Join now - it's free</span>
               <ArrowRight className="inline group-hover:translate-x-1 transition-all duration-300"  />
             </button>
-            <button className="border-2 hover:bg-gray-50 transition-colors duration-500 border-cyan-100 rounded-lg bg-white font-semibold py-2 text-lg shadow cursor-pointer">
+            <button className="hero-btn-signin">
               {" "}
               Sign In
             </button>
           </div>
 
           <div className="flex gap-x-2 text-sm text-gray-600">
-            <p className="flex gap-x-2 items-center">
+            <p className="flex-2-center">
               <Users size={18} />
               <span>1M+ professionals</span>
             </p>
-            <p className="flex gap-x-2 items-center">
+            <p className="flex-2-center">
               <Briefcase size={18} />
               <span>50K+ companies</span>
             </p>
           </div>
         </div>
-        <div></div>
+
+
+        <div className="bg-white shadow-lg rounded-lg ">
+            <div className="flex p-4 items-center gap-x-2">
+                <Users className="bg-green-400 rounded-full p-3" size={50}/>
+                <div>
+                    <h3 className="font-semibold">Professional Network</h3>
+                    <p className="text-sm text-gray-600">Connect with industry experts</p>
+                </div>
+            </div>
+
+        </div>
       </section>
     );
 }
