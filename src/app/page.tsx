bcc3f74a2682652e10288b24b24e98d690fade8a
Umer <omiomeromi11@gmail.com>
2025-11-14 09:57:52 +0300

    Add call to action section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { auth } from "@/lib/auth";
 import { signOutAction } from "./actions/auth";
 import NavigationBar from "@/components/navbar";
 import HeroSection from "@/components/herosection";
-import { Globe, Network, Target, Zap } from "lucide-react";
+import { ArrowRight, Globe, Network, Target, Zap } from "lucide-react";
 import WhyCard from "@/components/whycard";
 import CountUpData from "@/components/countupdata";
 
@@ -39,21 +39,43 @@ export default async function Home() {
             <WhyCard
               title="Targeted Opportunities"
               description="Discover personalized job opportunities and career advancement paths"
-              icon={<Target className="why-logo"  size={50} />}
+              icon={<Target className="why-logo" size={50} />}
             />
             <WhyCard
               title="Instand Insights"
               description="Share your expertise and stay updated with industry trends"
-              icon={<Zap className="why-logo" size={50}/>}
+              icon={<Zap className="why-logo" size={50} />}
             />
             <WhyCard
               title="Global Reach"
               description="Connect with professionals from companies around the world"
-              icon={<Globe className="why-logo" size={50}/>}
+              icon={<Globe className="why-logo" size={50} />}
             />
           </div>
         </div>
-        <CountUpData/>
+        <CountUpData />
+
+        <div
+          className="ready 
+        "
+        >
+          <h2 className="text-4xl font-bold lg:text-5xl max-w-4xl">
+            Ready to shape your professional story?
+          </h2>
+          <p className="text-xl max-w-4xl text-gray-600">
+            Join professionals who are already building their network and
+            advancing their careers on our platform
+          </p>
+          <div className="flex flex-col gap-3 bg items-center sm:flex-row">
+            <button className="hero-btn-signup px-4">
+              Get started today
+              <ArrowRight className="inline group-hover:translate-x-1 transition-all duration-300" />
+            </button>
+            <button className="hero-btn-signin px-4">
+              Already have an account?
+            </button>
+          </div>
+        </div>
         <div className="flex flex-col items-center justify-center h-screen gap-4">
           <h1 className="text-4xl font-bold">Cosden Solutions</h1>
           <div className="flex gap-4 mt-8">
