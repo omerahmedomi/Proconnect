e1660f18bcb5ab50793a25ad717f390e986c2e08
Umer <omiomeromi11@gmail.com>
2025-11-15 11:22:16 +0300

    Share AuthComponent to both auth pages and make the necessary changes

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,19 +1,6 @@
+import AuthComponent from "@/components/auth";
 import { signInAction } from "../../actions/auth";
 
 export default function SignInPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-2xl font-bold">Sign In</h1>
-      <form action={signInAction} className="flex flex-col gap-3 w-64">
-        <input type="email" name="email" placeholder="Email" required />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Sign In</button>
-      </form>
-    </div>
-  );
+  return <AuthComponent type={"signin"} />;
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,54 +1,9 @@
-import Link from 'next/link'
-import { signUpAction } from "../../actions/auth";
-import Password from "../../../components/password";
+
+import AuthComponent from '@/components/auth';
 
 export default function SignUpPage() {
   return (
-    <div className="flex flex-col items-center  justify-center  gap-4 px-18 py-10 w-full max-w-[555px] border mx-4 rounded-lg shadow-2xl border-blue-200">
-      <h1 className="text-3xl font-semibold text-center  text-gray-700">
-        Create a free account
-      </h1>
-      <p>
-        Already have an account?{" "}
-        <Link
-          href={"/signin"}
-          className="underline underline-offset-5 text-sm  text-blue-600"
-        >
-          Sign in
-        </Link>
-      </p>
-      <button className="border px-2 py-1 w-full rounded text-lg">
-        Sign up with Google
-      </button>
-      <div className="flex items-center justify-center gap-5 w-full text-gray-400">
-        <div className="w-4 h-px bg-gray-200 grow"></div>
-        <p>or</p>
-        <div className="w-4 h-px bg-gray-200 grow"></div>
-      </div>
-      <form action={signUpAction} className="flex flex-col gap-5  w-full">
-        <input
-          type="text"
-          name="name"
-          placeholder="Your Full Name"
-          className="auth-input "
-        />
-
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-          className="auth-input"
-        />
-
-        <Password type="password" />
-
-        <Password type="confirmPassword" />
-
-        <button type="submit" className="w-full p-3 bg-black rounded text-white font-semibold hover:bg-white hover:text-black cursor-pointer transition-colors border duration-300 active:text-black active:bg-white">
-          Sign Up
-        </button>
-      </form>
-    </div>
+    <AuthComponent type='signup'/>
+    
   );
 }
