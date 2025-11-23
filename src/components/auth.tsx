a94823b41527141b542106452675c58c6dbbfcbb
Umer <omiomeromi11@gmail.com>
2025-11-23 15:35:11 +0300

    Add google logo to google auth button

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -7,10 +7,11 @@ import {
 import Password from "./password";
 import Submit from "./submit";
 import AuthForm from "./authform";
+import GoogleIcon from "./icons/google";
 
 export default function AuthComponent({ type }: { type: string }) {
   return (
-    <div className="flex flex-col items-center  justify-center  gap-4 px-18 py-10 w-full max-w-[555px] border mx-4 rounded-lg shadow-2xl border-blue-200">
+    <div className="flex flex-col items-center  justify-center  gap-4 px-22  py-10 w-full max-w-[555px] border mx-4 rounded-lg shadow-2xl border-blue-200">
       <h1 className="text-3xl font-semibold text-center  text-gray-700">
         {type == "signup" ? "Create a free account" : "Sign in to your account"}
       </h1>
@@ -25,8 +26,9 @@ export default function AuthComponent({ type }: { type: string }) {
           {type == "signup" ? "Sign In" : "Sign Up"}
         </Link>
       </p>
-      <form action={signInGoogleAction}>
-        <button className="border px-2 py-1 w-full rounded text-lg">
+      <form action={signInGoogleAction} className="w-full">
+        <button className="border border-0.5 px-2 py-1 w-full rounded text-lg flex items-center gap-3 justify-center hover:bg-gray-50 transiton duration-200  cursor-pointer">
+          <GoogleIcon/>
           Sign {type.slice(-2)} with Google
         </button>
       </form>
